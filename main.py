4b7c3b749dd6cbf74a469d830eb03bd1071d0773
vgord4 <vgordon4@students.avc.edu>
2023-08-27 13:35:19 -0700

    As-2-1 Done

--- main.py
+++ main.py
@@ -5,6 +5,20 @@ def main():
     Use m_perc and f_perc for your results
     ##################################################
     """
+    
+    total = 100
+
+    num_male = int(input('How many males are in your class? '))
+    num_female = int(input('How many females are in your class? '))
+
+    m_perc = (num_male/total) * 100
+    f_perc = (num_female/total) * 100
+    
+
+    print (f'Total number of students: {total}')
+    print (f'Number of Male to Female students: {num_male}, {num_female}')
+    print (f'Male and female percentage: {m_perc:.2f}% {f_perc:.2f}%')
+
 
     """
     ########################################
